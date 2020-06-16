b91ec306f0a2b7072bbabadf83a48031f613a5b9
Mohith K Das <mohithkdas@msn.com>
2020-06-17 02:21:32 +0530

    added async await

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -3,8 +3,15 @@ const User = require('../src/models/user')
 
 // 5ee7d446cbcbb0ea9f68ceca
 
-User.findByIdAndUpdate('5ee7c002ab2dc9d3a3cfa080',{age: 1}).then((user)=>{
-    console.log(user)
-    return User.countDocuments({age:1})
-}).then((result)=>{console.log(result)}).catch((e)=>{console.log(e)})
+// User.findByIdAndUpdate('5ee7c002ab2dc9d3a3cfa080',{age: 1}).then((user)=>{
+//     console.log(user)
+//     return User.countDocuments({age:1})
+// }).then((result)=>{console.log(result)}).catch((e)=>{console.log(e)})
 
+const updateAgeandCount = async (id,age) => {
+    const user = await User.findByIdAndUpdate(id,{age : age})
+    const count = await User.countDocuments({age})
+    return count
+}
+
+updateAgeandCount('5ee609ace28b21273187aacd',1).then((r)=>{console.log(r)}).catch((e)=>{console.log(e)})
--- playground/promise-chaining2.js
+++ playground/promise-chaining2.js
@@ -3,8 +3,15 @@ const Task = require('../src/models/tasks')
 
 
 
-Task.findByIdAndDelete('5ee7d97a59b723ef40431e4a').then((task)=>{
-    console.log(task)
-    return Task.countDocuments({completed: false})
-}).then((result)=>{console.log(result)}).catch((e)=>{console.log(e)})
+// Task.findByIdAndDelete('5ee7d97a59b723ef40431e4a').then((task)=>{
+//     console.log(task)
+//     return Task.countDocuments({completed: false})
+// }).then((result)=>{console.log(result)}).catch((e)=>{console.log(e)})
 
+const deleteTaskandCount = async (id)=>{
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({compeleted : false})
+    return count
+}
+
+deleteTaskandCount('5ee7d99259b723ef40431e4b').then((r)=>{console.log(r)}).catch((e)=>{console.log(e)})
